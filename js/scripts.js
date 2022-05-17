ee91629557891e1c5f1e0469ded5c63415af0ba3
German Baena <germanb66@gmail.com>
2022-05-17 13:52:25 -0500

    Actualización general

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,47 @@
-const openModal = document.querySelector('.wrapper__button'),
+const buttonOpenModal = document.getElementById('button-open-modal'),
     modal = document.querySelector('.modal'),
     modalContent = modal.querySelector('.modal__content'),
-    openOtherModal = document.querySelector('.modal__button'),
+    buttonOpenOtherModal = document.getElementById('button-open-other-modal'),
     modalNew = document.querySelector('.modalnew'),
     modalNewContent = document.querySelector('.modalnew__content'),
-    modalNewButton = document.querySelector('.modalnew__button')
+    buttonCloseOtherModal = document.getElementById('button-close-other-modal')
 
 
-openModal.addEventListener('click', e => {
+// Función abril primer modal
+const openModal = e => {
     e.stopPropagation()
     modal.style.visibility = 'visible';
     modalContent.classList.add('modal__show')
-})
+}
+buttonOpenModal.addEventListener('click', openModal)
 
-modal.addEventListener('click', e => {
+
+// Función cerrar primer modal
+const closeModal = e => {
     e.stopPropagation()
-    if (e.target.classList.contains('modal')) {
+    if (e.target.classList.contains('modal') || (e.target.classList.contains('modalnew__button'))) {
         modalContent.classList.remove('modal__show')
         modal.style.visibility = 'hidden'
     }
-})
+}
+modal.addEventListener('click', closeModal)
 
-openOtherModal.addEventListener('click', e => {
+
+// Función abrir segundo modal
+const openOtherModal = e => {
     e.stopPropagation()
     modalNew.style.visibility = 'visible'
     modalNewContent.classList.add('modalnew__show')
-})
+}
+buttonOpenOtherModal.addEventListener('click', openOtherModal)
 
-modalNewButton.addEventListener('click', e => {
+
+// Función cerrar segundo modal
+const closeOtherModal = e => {
     e.stopPropagation()
     modalNewContent.classList.remove('modalnew__show')
     modalNew.style.visibility = 'hidden'
-})
+    closeModal(e)
+}
+buttonCloseOtherModal.addEventListener('click', closeOtherModal)
 
